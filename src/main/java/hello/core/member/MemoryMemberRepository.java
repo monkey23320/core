de5db006eb57a7537c2b5874a98d3a14172e0c08
Heonseo <gkgjstj8210@gmail.com>
2022-09-06 15:16:39 +0900

    Auto Injection

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     // 실무에선 동시성 이슈로 인한 ConcurrenationMap 사용
